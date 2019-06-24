52c27be81e5ac6b02eb4ecfdc16169ae38baf6c7
Thomas <thom.rebaud@hotmail.com>
2019-06-24 15:36:31 -0400

    support command line args

--- cli.js
+++ cli.js
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-const pickmoji = require('./pickmoji');
+const importJsx = require('import-jsx');
+const emoji = require('node-emoji');
+const clipboardy = require('clipboardy');
 
+const { log } = console;
 const [,, ...args] = process.argv;
-pickmoji(args[0]);
+
+if (args.length) {
+  const emojisObj = emoji.search(args[0]);
+  if (emojisObj.length === 1 && emojisObj[0].key === args[0]) {
+    const foundEmoji = emojisObj[0].emoji;
+    clipboardy.writeSync(foundEmoji);
+
+    log();
+    log(`${foundEmoji} copied to clipboard`);
+    process.exit();
+  } else {
+    log('\n404 Not Found\n')
+  }
+}
+
+importJsx('./Pickmoji');
